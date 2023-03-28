c8f1a09e1ba704a1d53459fdcddc72122877a370
zpatricklee <zpatricklee@gmail.com>
2023-03-27 17:54:27 -0700

    added react strict mode

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <BrowserRouter>
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
+  // <BrowserRouter>
   // </BrowserRouter>
 );
